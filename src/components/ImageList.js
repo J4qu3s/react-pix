91031953acbbdc0c39be6f725c138eb1977ec41e
J4qu3s <jarkko.hoyhtya@gmail.com>
2021-11-01 22:37:24 +0200

    done to 111
    Adding more styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ImageCard from "./ImageCard";
 import './imageList.css';
 
 const ImageList = (props) => {
 
     // Destructive on image to object instead of using "image" from props.
-    const images =  props.images.map(({description, id, urls}) => {
-        return <img key={id} src={urls.regular} alt={description}/>
+    const images =  props.images.map((image) => {
+        return <ImageCard key={image.id} image={image}/>
     });
 
     return (
